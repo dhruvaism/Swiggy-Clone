6d49aa6077cc272fa2f7d03025f83a9404ee55ce
dhruvaism <b170177@nitsikkim.ac.in>
2021-04-08 17:17:17 +0530

    initial commit

---
+++ src/main/java/com/swiggy/clone/SwiggyCloneApplication.java
@@ -0,0 +1,14 @@
+package com.swiggy.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiggyCloneApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiggyCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swiggy/clone/SwiggyCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiggy.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiggyCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
