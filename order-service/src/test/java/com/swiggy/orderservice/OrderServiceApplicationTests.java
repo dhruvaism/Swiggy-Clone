3255fa187d19eb799b70627a48f8a19d5c9cd018
dhruvaism <b170177@nitsikkim.ac.in>
2021-04-09 16:28:34 +0530

    order-service added

---
+++ order-service/src/test/java/com/swiggy/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiggy.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
